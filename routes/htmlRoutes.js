d86fa660aa618afd6793e353465601f7cbd57206
EFerri222 <eferri222@gmail.com>
2019-03-07 20:12:32 -0500

    added route files, changed name of old route files to have Old at the end

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,42 @@
-var db = require("../models");
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  // Route to get existing users
+  app.get("/users", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/users.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Route to get new user form
+  app.get("/users/new", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/new-user.html"));
   });
 
-  // Render 404 page for any unmatched routes
+  // Route to get drug name form
+  app.get("/drug/new", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/new-drug.html"));
+  });
+
+  // Route to get interaction results
+  app.get("/drug/interaction-results", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/interaction-results.html"));
+  });
+
+  // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
